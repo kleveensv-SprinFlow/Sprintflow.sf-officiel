3bb5720b8e5bb9f93a6dd695e4e555a6f75d3511
kleveensv-SprinFlow <kleveensv@gmail.com>
2025-10-29 12:46:39 +0100

    Updated categories.ts

---
+++ src/data/categories.ts
@@ -0,0 +1,32 @@
+export const EXERCISE_CATEGORIES = [
+  {
+    key: 'halterophilie',
+    label: 'Haltérophilie',
+    icon: '🏋️',
+  },
+  {
+    key: 'muscu_bas',
+    label: 'Muscu. Bas',
+    icon: '🦵',
+  },
+  {
+    key: 'muscu_haut',
+    label: 'Muscu. Haut',
+    icon: '💪',
+  },
+  {
+    key: 'unilateral',
+    label: 'Unilatéral',
+    icon: '🚶',
+  },
+  {
+    key: 'pliometrie',
+    label: 'Pliométrie',
+    icon: '🏃',
+  },
+  {
+    key: 'lancers',
+    label: 'Lancers',
+    icon: '🎯',
+  },
+];
