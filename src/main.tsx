7cc734ed833c5937adacc227c7a64c9afa8be711
kleveensv-SprinFlow <kleveensv@gmail.com>
2025-11-03 14:17:25 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { AuthProvider } from './hooks/useAuth';
 
 createRoot(document.getElementById('root')!).render(
-  <App />
+  <AuthProvider>
+    <App />
+  </AuthProvider>
 );
