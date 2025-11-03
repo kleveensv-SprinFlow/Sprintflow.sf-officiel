d8ee14406a15ff6b53196458901f49eaa59e912b
kleveensv-SprinFlow <kleveensv@gmail.com>
2025-11-03 07:16:40 +0100

    Updated workoutTypes.ts

---
+++ src/data/workoutTypes.ts
@@ -0,0 +1,22 @@
+export const DEFAULT_WORKOUT_TYPES = [
+  {
+    id: 'aerobie',
+    name: 'Aérobie',
+    color: '#3498db', // Bleu
+  },
+  {
+    id: 'lactique',
+    name: 'Lactique',
+    color: '#e67e22', // Orange
+  },
+  {
+    id: 'lactique_cote',
+    name: 'Lactique Côte',
+    color: '#c0392b', // Rouge
+  },
+  {
+    id: 'musculation',
+    name: 'Musculation/Haltérophilie',
+    color: '#34495e', // Gris Anthracite
+  },
+];
