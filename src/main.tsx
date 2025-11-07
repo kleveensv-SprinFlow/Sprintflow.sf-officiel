b142e216b68c28acba48946090d2330dd456df96
kleveensv-SprinFlow <kleveensv@gmail.com>
2025-11-07 09:49:23 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import App from './App.tsx';
 import './index.css';
 import { AuthProvider } from './hooks/useAuth.tsx';
 
+if (
+  localStorage.theme === 'dark' ||
+  (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+  document.documentElement.classList.add('dark');
+} else {
+  document.documentElement.classList.remove('dark');
+}
+
 createRoot(document.getElementById('root')!).render(
   <AuthProvider>
     <App />
