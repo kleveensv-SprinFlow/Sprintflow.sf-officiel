3177829dede29a552105a3739661f2877789c4c1
kleveensv-SprinFlow <kleveensv@gmail.com>
2025-11-22 22:49:51 +0000

    Updated PlanningPage.tsx

--- src/pages/PlanningPage.tsx
+++ src/pages/PlanningPage.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { AthletePlanning } from '../components/planning/AthletePlanning';
+import { CoachPlanning } from '../components/planning/CoachPlanning';
 import useAuth from '../hooks/useAuth';
 
 const PlanningPage: React.FC = () => {
   const { profile } = useAuth();
+
+  if (profile?.role === 'coach') {
+    return <CoachPlanning />;
+  }
 
   return <AthletePlanning />;
 };
