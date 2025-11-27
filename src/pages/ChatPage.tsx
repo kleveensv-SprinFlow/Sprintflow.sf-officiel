f361c71c25b0974618883caaa5ffa2b399420e6b
kleveensv-SprinFlow <kleveensv@gmail.com>
2025-11-27 10:58:52 +0000

    Updated ChatPage.tsx

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,8 +1,15 @@
+// src/pages/ChatPage.tsx
 import React from 'react';
-import ChatManager from '../components/chat/ChatManager';
+import SprintyChatView from '../components/chat/sprinty/SprintyChatView';
 
 const ChatPage: React.FC = () => {
-  return <ChatManager />;
+  return (
+    // On utilise 'fixed inset-0' et un z-index élevé pour que cette page 
+    // passe par dessus le layout standard et son header
+    <div className="fixed inset-0 z-50 bg-sprint-dark-background flex flex-col">
+      <SprintyChatView />
+    </div>
+  );
 };
 
 export default ChatPage;
