3dbce0ad7143bebee25c9027c67c2df8916d69ce
kleveensv-SprinFlow <kleveensv@gmail.com>
2025-12-02 14:31:05 +0000

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,8 @@ import './index.css';
 import { AuthProvider } from './hooks/useAuth.tsx';
 import { routes } from './routes/index.tsx';
 
-// Thème initial pour éviter le flash
-const theme = localStorage.getItem('theme');
-if (
-  theme === 'dark' ||
-  (theme === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches) ||
-  (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches)
-) {
-  document.documentElement.classList.add('dark');
-} else {
-  document.documentElement.classList.remove('dark');
-}
+// On s'assure que le mode sombre est actif (ceinture et bretelles avec index.html)
+document.documentElement.classList.add('dark');
 
 createRoot(document.getElementById('root')!).render(
   <AuthProvider>
